735740b028ff86b4f732081cbbce728671ecb891
udayempire <kuday0726@gmail.com>
2026-08-12 22:33:29 +0530

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,15 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Bhojpuri Stars",
   description: "",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
